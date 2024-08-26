428cfcd45608fe8023c9c021def22a5a7edf3bb1
Matt Q <neato0079@gmail.com>
2024-08-26 06:09:48 -0700

    init exam_analytics app and map urls

--- exam_analytics/asgi.py
+++ exam_analytics/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for exam_app project.
+ASGI config for exam_analytics project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'exam_app.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'exam_analytics.settings')
 
 application = get_asgi_application()
--- exam_analytics/wsgi.py
+++ exam_analytics/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for exam_app project.
+WSGI config for exam_analytics project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'exam_app.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'exam_analytics.settings')
 
 application = get_wsgi_application()
