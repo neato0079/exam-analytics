e9c3ee1d5e7302fef0ba4a9d634d323d23d7afbe
Matt Q <neato0079@gmail.com>
2024-09-25 03:12:48 -0700

    add handler for home.html

--- exam_graph/views.py
+++ exam_graph/views.py
@@ -3,7 +3,8 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>HOME</h1>')
+    return render(request, 'home.html')
+    # return HttpResponse('<h1>asdfasdfasdfasdf</h1>')
 
 def not_home(request):
     return HttpResponse('<h1>NOT HOME</h1>')
