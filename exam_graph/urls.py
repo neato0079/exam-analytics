62353351cec1fa1fc13a3090c58c8b4330a379dc
Matt Q <neato0079@gmail.com>
2024-12-16 04:16:02 -0800

    allow 'results/' url pattern to accept additional aruguments to pass on to relevent views.py

--- exam_graph/urls.py
+++ exam_graph/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('help/', views.help, name='help'),
     path('', views.home, name='thehome'),
     path('test/', views.result_graph, name='test'),
-    path('result/', views.upload_csv, name='result')
+    path('result/<str:modality>/', views.upload_csv, name='result')
 ]
