25812b0a63825fa51dcf091ae8e38041c7537ac3
Matt Q <neato0079@gmail.com>
2024-12-18 08:34:36 -0800

    rework filter definitions

--- exam_graph/filters.py
+++ exam_graph/filters.py
@@ -1,3 +1,43 @@
+
+filters = {
+    "date_range": {
+        "start": "2024-01-01",
+        "end": "2024-12-31"
+    },
+    
+    "x_axis": { # Time filters
+        "group_by": "month",  # Options: "day", "month", "year"
+        "shift_filter": ["AM", "PM", "NOC"],  # List of shifts to include
+        "week_view": None  # Options: "weekends", "weekdays", None
+    },
+
+    "y_axis": { # Metrics
+        "metric": "number_of_exams",  # Options: "number_of_exams", "exam_start_to_finish_time", etc.
+    },
+
+    "exam_filters": {
+        "modalities": ["CT", "MRI"],  # List of selected modalities
+        "exam_name": "Head CT"  # Specific exam name
+    },
+
+    "shift_color_indicators": False,
+}
+
+# VALID OPTIONS FOR FILTERS:
+
+metric_options = [
+    'number_of_exams',
+    'exam_start_to_finish_time'
+    'ratio_of_completed_exams_to_ordered_exams'
+]
+
+groupby_options = [
+    'hour'
+    'day'
+    'month',
+    'year'
+]
+
 modalities = [
     'XR',
     'CT',
@@ -6,52 +46,8 @@ modalities = [
     'NM'
     ]
 
-filters = {
-    'date_range': {
-        # make this ISO. edit later. sample data set is small enough for this not to matter yet
-        'start': '00:00:0000',
-        'end': '00:00:0000'
-    },
-
-    'shift_color_indicators': False,
-
-    'x_plot_filters': {
-        'period_increments':{
-            'day': False,
-            'month': True,
-            'year': False,
-        },
-        'period_filters': {
-            'shift' : {
-                'AM': True,
-                'PM': True,
-                'NOC': True,
-            },
-        'weekends only': False,
-        'weekdays only': False,
-        },
-        'exam_filters': {
-            'modalities': [],
-            'exam_name':'',
-        }
-    },
-
-    'y_plot_filters': {
-        'number_of_exams': True,
-        'exam_start_to_finish_time': '00:00',
-        'ratio_of_completed_exams_to_ordered_exams': {
-            'completed': 0,
-            'ordered':0
-        },
-    },
-}
-
-filter_selections = {
-    "x_axis": "Month",
-    "y_axis": "Exam Count",
-    "modality": "CT",
-    "date_range": ("2024-01", "2024-12"),
-    "view_shifts": False,
-    "only_weekends": False,
-    "only_weekdays": False
-}
+week_view_options = [
+    'weekends',
+    'weekdays',
+    None
+]
