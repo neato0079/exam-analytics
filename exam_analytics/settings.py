0ca00d324d54eca2d3c004da6ed710c431835e8f
Matt Q <neato0079@gmail.com>
2025-01-03 20:54:20 -0800

    generate new secret key and store off repo

--- exam_analytics/settings.py
+++ exam_analytics/settings.py
@@ -11,6 +11,14 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
+
+# Create a Config object to access environment variables
+# config = Config('./.env')
+
+# Get the SECRET_KEY from the .env file
+SECRET_KEY = config('SECRET_KEY')
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -18,12 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ba&&05)-29=638q95n^waline)j13_m=3$8o2q2#zl%-8v10(^'
+DEBUG = config('DEBUG', cast=bool)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+
 
 ALLOWED_HOSTS = []
 
