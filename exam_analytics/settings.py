6ae5761ef2a7882dd152555624c31b0200b5990f
Matt Q <neato0079@gmail.com>
2025-01-03 22:21:53 -0800

    add allowed hosts

--- exam_analytics/settings.py
+++ exam_analytics/settings.py
@@ -31,7 +31,7 @@ DEBUG = config('DEBUG', cast=bool)
 # SECURITY WARNING: don't run with debug turned on in production!
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://exam-analytics.onrender.com']
 
 
 # Application definition
