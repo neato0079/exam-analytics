88f85738a8fe351436a00137530307be77f63a10
Matt Q <neato0079@gmail.com>
2025-01-07 03:29:11 -0800

    rename url for results page. also rename home page name

--- exam_graph/urls.py
+++ exam_graph/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('help/', views.help, name='help'),
     path('', views.home, name='thehome'),
-    path('test/', views.filter_submission_handler, name='test'),
+    path('result/', views.filter_submission_handler, name='test'),
     path('form/', views.form_page, name='formyayay'),
     path('result/<str:modality>/', views.upload_csv, name='result'),
     path('mock_data/', views.display_mock_csv, name='mock'),
