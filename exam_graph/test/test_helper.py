917b728425b8d7edefd508cfb50e915ef8e5e1ef
Matt Q <neato0079@gmail.com>
2025-01-18 03:37:26 -0800

    remove unittest code since we're sticking with pytest

--- exam_graph/test/test_helper.py
+++ exam_graph/test/test_helper.py
@@ -1,30 +1,6 @@
-# import unittest
-# import os 
-# import sys
-# print("Current Working Directory:", os.getcwd())
 from pathlib import Path
-# # sys.path.append(str(Path(__file__).resolve().parent.parent))
-# print("Python Path:", sys.path)
-# # print(os.environ)
-# # import helper
 from exam_graph import helper
 from datetime import datetime
-
-# # run tests with 'python -m unittest discover'     
-# class TestHelp(unittest.TestCase):
-
-#     def test_pickle_copy(self):
-
-#         pickle_fp = Path('/home/data.pickle')
-#         new_pickle_fp = helper.pickle_copy(pickle_fp)
-#         expected_fp = Path('/home/data(1).pickle')
-
-#         self.assertNotEqual(pickle_fp, new_pickle_fp)
-#         self.assertEqual(new_pickle_fp, pickle_fp)
-
-
-# if __name__ == '__main__':
-#     unittest.main()
 
 def test_pickle_copy():
     pickle_fp = Path('/home/data.pickle')
