0787e32f921c8d77906863869245434c5e0c69f1
Matt Q <neato0079@gmail.com>
2025-01-18 00:46:55 -0800

    finish test for pickle copy

--- exam_graph/test/test_helper.py
+++ exam_graph/test/test_helper.py
@@ -3,18 +3,23 @@ import os
 import sys
 print("Current Working Directory:", os.getcwd())
 from pathlib import Path
-sys.path.append(str(Path(__file__).resolve().parent.parent))
+# sys.path.append(str(Path(__file__).resolve().parent.parent))
 print("Python Path:", sys.path)
 # print(os.environ)
-import helper
+# import helper
+from exam_graph import helper
 
 # run tests with 'python -m unittest discover'     
 class TestHelp(unittest.TestCase):
 
     def test_pickle_copy(self):
+
         pickle_fp = Path('/home/data.pickle')
         new_pickle_fp = helper.pickle_copy(pickle_fp)
+        expected_fp = Path('/home/data(1).pickle')
+
         self.assertNotEqual(pickle_fp, new_pickle_fp)
+        self.assertEqual(new_pickle_fp, expected_fp)
 
 
 if __name__ == '__main__':
