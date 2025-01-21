853995a6e8c91c378754cd1a1ec4b68e0652547a
Matt Q <neato0079@gmail.com>
2025-01-21 09:25:01 -0800

    add test for build_usr_config

--- exam_graph/test/test_helper.py
+++ exam_graph/test/test_helper.py
@@ -1,6 +1,10 @@
 from pathlib import Path
 from exam_graph import helper
 from datetime import datetime
+from . import mock_data_gen
+import json
+import shutil
+# Run with 'pytest', 'coverage report', and 'coverage html'
 
 def test_pickle_copy():
     pickle_fp = Path('/home/data.pickle')
@@ -29,11 +33,73 @@ def test_get_shift():
     assert helper.get_shift(nc_sample2) == 'NOC'
 
 
-def mock_config_fp(tmp_path_factory):
-    pass
-    # fp = 
+# def mock_config_fp(tmp_path_factory):
+#     pass
+#     # fp = 
 
 
 
-def test_build_usr():
-    pass
+# def test_build_usr():
+#     pass
+
+
+
+
+def test_build_usr_config(tmp_path):
+
+    # set dir and fp
+    dir:Path = tmp_path / 'prop'
+    pickle_fn = 'test.pickle'
+    fp = dir / 'test_config.json'
+
+    # set content in test json
+    expected_content = {
+        'user datasets' : [pickle_fn]
+    }
+
+    # test in absense of config fp
+    helper.build_usr_config('test.pickle', fp)
+
+    # read into fp and check content against expected content
+    with open(fp, 'r') as json_file:
+        actual_content = json.load(json_file)
+    assert actual_content == expected_content, f"Expected {expected_content}, but got {actual_content}"
+
+
+    # test in absense of fp
+    shutil.rmtree(dir)
+    dir.mkdir(parents=True, exist_ok=True)
+    helper.build_usr_config('test.pickle', fp)
+
+    # read into fp and check content against expected content
+    with open(fp, 'r') as json_file:
+        actual_content = json.load(json_file)
+    assert actual_content == expected_content, f"Expected {expected_content}, but got {actual_content}"
+
+
+    # test where fp is presesnt. json should be updated rather than created
+    shutil.rmtree(dir) # reset path
+    print(f'After shutil.rmtree is called, dir is now:{dir.exists()}')
+    dir.mkdir(parents=True, exist_ok=True) # create path
+
+
+    # write file again
+    with fp.open("w") as f:
+        json.dump(expected_content, f, indent=4) 
+
+    helper.build_usr_config('test(1).pickle', fp)
+
+    # set new content in test json
+    expected_new_content = {
+        'user datasets' : [pickle_fn, 'test(1).pickle']
+    }
+
+    # read json and check for match
+    with open(fp, 'r') as json_file:
+        actual_content = json.load(json_file)
+    assert actual_content == expected_new_content, f"Expected {expected_new_content}, but got {actual_content}"
+    # dir.mkdir()
+    # fp.write_text(CONTENT, encoding="utf-8")
+    # assert fp.read_text(encoding="utf-8") == CONTENT
+    # assert len(list(tmp_path.iterdir())) == 1
+    # shutil.rmtree(dir)
