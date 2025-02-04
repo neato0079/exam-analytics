97590930ff35aaa2acbf48b81e80f22a49294a81
Matt Q <neato0079@gmail.com>
2025-02-04 13:17:45 -0800

    add missing migration build commands for render deploy duh

--- build.sh
+++ build.sh
@@ -5,3 +5,8 @@ pip install -r requirements.txt
 
 # Collect static files
 python manage.py collectstatic --noinput
+
+# Update DB with migrations
+
+python manage.py makemigrations
+python manage.py migrate
