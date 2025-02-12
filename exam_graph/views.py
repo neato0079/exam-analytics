8c3cdeddfcc9c0158e671765ed8c04d17eaf7104
Matt Q <neato0079@gmail.com>
2025-02-12 09:31:20 -0800

    remove redundant code

--- exam_graph/views.py
+++ exam_graph/views.py
@@ -112,7 +112,7 @@ def upload_csv(request):
 
 def filter_submission_handler(request:HttpRequest):
 
-    # parsed_mocked_data = helper.build_test_master_json_df()
+    # get requested pickel server fp and convert it to a df
     pickle_fp:Path = helper.selected_pickle_fp(USER_CONFIG_FP, DATASET_DIR)
     df = helper.pickle_to_df(pickle_fp)
     print('Filtered from source:')
@@ -121,34 +121,22 @@ def filter_submission_handler(request:HttpRequest):
     if request.method == 'POST':
         try:
 
-            # parse filter request
-            filtr:FilterRequest = FilterRequest(request.POST)  # returns a class instance containing the necessary arguments for master_filter()
-
-            # df = filter_params['source_dataframe']
-            period = filtr.period
-            modality_lst = filtr.modalities
-            metric = filtr.metric
-            daterange = filtr.date_range
-            datestr = filtr.date_range_string
-            shift_view = filtr.shift_view
-
-            # TESTING SHIFT PLOT. TOTALS ONLY
-
+            # set our post request to an instance of FilterRequest for parsing
+            filtr:FilterRequest = FilterRequest(request.POST)  
 
             # set summary tables:
             summary_tables = []
 
-            if shift_view:
-                # axes_data:pd.DataFrame
+            if filtr.shift_view:
+                # axes_data:pd.DataFrame | pd.Series
                 ratio_data, axes_data = filters.master_filter(df, filtr)
 
-
                 # create shift view graph
-                general = myplot.plot_shift(axes_data,period)
+                general = myplot.plot_shift(axes_data,filtr.period)
                 graph_base64 = [general]
 
                 # create ratio graph only on total metric
-                if metric == 'totals':
+                if filtr.metric == 'totals':
                     ratio = myplot.plot_ratios(ratio_data)
                     graph_base64.append(ratio)
 
@@ -161,7 +149,7 @@ def filter_submission_handler(request:HttpRequest):
             else:
                 axes_data:pd.Series = filters.master_filter(df, filtr)
                 # graph without shift view
-                graph_base64 = [myplot.gen_encoded_graph(axes_data, period, metric, modality_lst)]
+                graph_base64 = [myplot.gen_encoded_graph(axes_data, filtr.period, filtr.metric, filtr.modalities)]
 
                 # build summary
                 summary = DataSummary(axes_data)
@@ -171,18 +159,17 @@ def filter_submission_handler(request:HttpRequest):
 
             stuff_for_html_render = {
                 'graphs': graph_base64,
-                'selected_period': period,
-                'selected_modality': modality_lst,
-                'selected_metric': metric,
-                'start_date': datestr[0],
-                'end_date': datestr[1],
-                'shift_view': shift_view,
+                'selected_period': filtr.period,
+                'selected_modality': filtr.modalities,
+                'selected_metric': filtr.metric,
+                'start_date': filtr.date_range_string[0],
+                'end_date': filtr.date_range_string[1],
+                'shift_view': filtr.shift_view,
                 'summary': summary_tables,
                 'dataset_name': pickle_fp.stem
             }
     
             return render(request, 'form.html', stuff_for_html_render)
-
 
         except Exception as e:
             error_message = f"An error occurred: {e}"
